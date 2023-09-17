7d06e0829f19697b59ecae5bd294cbd9060cbfbb
Eliseu Codinhoto <zeucxb@gmail.com>
2023-09-17 12:36:22 -0300

    Initial Commit

---
+++ HelloWatch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HelloWatch Watch App
+//
+//  Created by Eliseu Codinhoto on 17/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWatch Watch App/HelloWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWatchApp.swift
+//  HelloWatch Watch App
+//
+//  Created by Eliseu Codinhoto on 17/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
